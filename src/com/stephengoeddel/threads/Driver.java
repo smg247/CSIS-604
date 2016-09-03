5ad0cb0d6fa71d6978abdf780146eff2bf9bb13e
Stephen Goeddel <sgoeddel247@gmail.com>
2016-09-03 01:40:38 -0400

    Able to sort with sockets.

--- src/com/stephengoeddel/threads/Driver.java
+++ src/com/stephengoeddel/threads/Driver.java
@@ -1,34 +1,55 @@
 package com.stephengoeddel.threads;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Driver {
+    public static int SERVER_PORT = 8000;
+
     private static String UNSORTED_NUMBERS_FILENAME = "unsorted-numbers.txt";
     private static String SORTED_NUMBERS_FILENAME = "sorted-numbers.txt";
+    private static String SERVER_ADDRESS = "127.0.0.1";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         boolean generateFile = args[0] != null ? Boolean.valueOf(args[0]) : true;
         boolean usingThreads = args[1] != null ? Boolean.valueOf(args[1]) : true;
         final int amountOfNumbers = args[2] != null ? Integer.parseInt(args[2]) : 1000;
-        final int numberOfThreads = args[3] != null ? Integer.parseInt(args[3]) : 3;
+        final int numberOfThreadsOrPorts = args[3] != null ? Integer.parseInt(args[3]) : 3;
+
+        List<Integer> serverPorts = new ArrayList<>();
+        if (!usingThreads) {
+            List<Object> argList = Arrays.asList(args);
+            for (Object arg : argList.subList(4, argList.size())) {
+                serverPorts.add(Integer.parseInt(arg.toString()));
+            }
+        }
 
         if (generateFile) {
             createRandomNumbersFile(amountOfNumbers);
         }
 
-        List<List<Integer>> subLists = createSubLists(amountOfNumbers, numberOfThreads);
+        List<List<Integer>> subLists = createSubLists(amountOfNumbers, numberOfThreadsOrPorts);
 
-        List<Integer> mergeResult;
+        List<Integer> mergeResult = null;
         if (usingThreads) {
-            List<Thread> threads = sortByThreads(subLists);
-            mergeResult = mergeResultsFromThreads(threads, subLists);
+            try {
+                List<Thread> threads = sortByThreads(subLists);
+                for (Thread thread : threads) {
+                    thread.join();
+                }
+
+                mergeResult = mergeSort(subLists);
+            } catch (InterruptedException e) {
+                System.out.println("Error while thread merging: " + e.getMessage());
+            }
         } else {
-            mergeResult = null;
+            List<List<Integer>> sortedSubLists = sortBySockets(subLists, serverPorts);
+            mergeResult = mergeSort(sortedSubLists);
         }
         
         if (mergeResult != null) {
@@ -81,24 +102,47 @@ public class Driver {
         return threads;
     }
 
-    private static List<Integer> mergeResultsFromThreads(List<Thread> threads, List<List<Integer>> subLists) {
-        List<Integer> mergeResult = null;
-        try {
-            for (Thread thread : threads) {
-                thread.join();
-            }
-
+    private static List<List<Integer>> sortBySockets(List<List<Integer>> subLists, List<Integer> serverPorts) throws IOException {
+        if (subLists.size() == serverPorts.size()) {
+            List<List<Integer>> sortedSubLists = new ArrayList<>();
             for (int i = 0; i < subLists.size(); i++) {
-                if (i == 0) {
-                    mergeResult = new ArrayList<>(subLists.get(i));
-                } else {
-                    mergeResult = merge(mergeResult, subLists.get(i));
+                List<Integer> subList = subLists.get(i);
+                int serverPort = serverPorts.get(i);
+                Socket socket = new Socket(SERVER_ADDRESS, serverPort);
+                BufferedReader inputReader = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+
+                for (Integer integer : subList) {
+                    printWriter.println(integer);
                 }
+                printWriter.println("."); // The period allows the SortingServer to know we are out of input for it
+
+                List<Integer> sortedSubList = new ArrayList<>();
+                String line;
+                while ((line = inputReader.readLine()) != null) {
+                    sortedSubList.add(Integer.parseInt(line));
+                }
+
+                sortedSubLists.add(sortedSubList);
             }
 
-        } catch (Exception e) {
-            System.out.println("Oops..exception during thread merging: " + e.getMessage());
+            return sortedSubLists;
+        } else {
+            throw new IllegalArgumentException("SubList size does not match the amount of servers to sort with.");
         }
+    }
+
+    private static List<Integer> mergeSort(List<List<Integer>> subLists) {
+        List<Integer> mergeResult = null;
+        for (int i = 0; i < subLists.size(); i++) {
+            if (i == 0) {
+                mergeResult = new ArrayList<>(subLists.get(i));
+            } else {
+                mergeResult = merge(mergeResult, subLists.get(i));
+            }
+        }
+
         return mergeResult;
     }
 
@@ -117,7 +161,6 @@ public class Driver {
 
     private static void createRandomNumbersFile(int amountOfNumbers) {
         try {
-            // I am not going to write random numbers into a file by hand...
             PrintWriter printWriter = new PrintWriter(UNSORTED_NUMBERS_FILENAME, "UTF-8");
 
             for (int i = 0; i < amountOfNumbers; i++) {
---
+++ src/com/stephengoeddel/threads/SortingServer.java
@@ -0,0 +1,45 @@
+package com.stephengoeddel.threads;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortingServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(Driver.SERVER_PORT);
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                try {
+                    BufferedReader inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    List<Integer> numbers = new ArrayList<>();
+                    String line;
+                    while ((line = inputReader.readLine()) != null && !".".equals(line)) {
+                        numbers.add(Integer.parseInt(line));
+                    }
+
+                    new Sorter(numbers).run();
+
+                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+                    for (Integer number : numbers) {
+                        printWriter.println(number);
+                    }
+                } finally {
+                    System.out.println("Closing socket");
+                    socket.close();
+                }
+            }
+        } finally {
+            System.out.println("Closing server socket");
+            serverSocket.close();
+        }
+    }
+}
