5281d7ac10c813de22fed2fdef1dcc3c4cd353c7
Stephen Goeddel <sgoeddel247@gmail.com>
2016-09-02 23:43:11 -0400

    Refactored code to support the switch to VM's

--- src/com/stephengoeddel/threads/Driver.java
+++ src/com/stephengoeddel/threads/Driver.java
@@ -4,34 +4,42 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Driver {
-    private static int AMOUNT_OF_NUMBERS = 1000;
-    private static int NUMBER_OF_THREADS = 3;
     private static String UNSORTED_NUMBERS_FILENAME = "unsorted-numbers.txt";
     private static String SORTED_NUMBERS_FILENAME = "sorted-numbers.txt";
 
     public static void main(String[] args) {
-        boolean generateFile = Boolean.valueOf(args[0]);
+        boolean generateFile = args[0] != null ? Boolean.valueOf(args[0]) : true;
+        boolean usingThreads = args[1] != null ? Boolean.valueOf(args[1]) : true;
+        final int amountOfNumbers = args[2] != null ? Integer.parseInt(args[2]) : 1000;
+        final int numberOfThreads = args[3] != null ? Integer.parseInt(args[3]) : 3;
+
         if (generateFile) {
-            try {
-                // I am not going to write random numbers into a file by hand...
-                PrintWriter printWriter = new PrintWriter(UNSORTED_NUMBERS_FILENAME, "UTF-8");
-
-                for (int i = 0; i < AMOUNT_OF_NUMBERS; i++) {
-                    printWriter.println(ThreadLocalRandom.current().nextInt(0, AMOUNT_OF_NUMBERS * 10));
-                }
-
-                printWriter.close();
-            } catch (Exception e) {
-                System.out.println("Oops...exception during number generation: " + e.getMessage());
-            }
+            createRandomNumbersFile(amountOfNumbers);
         }
 
-        List<Thread> threads = new ArrayList<>();
+        List<List<Integer>> subLists = createSubLists(amountOfNumbers, numberOfThreads);
+
+        List<Integer> mergeResult;
+        if (usingThreads) {
+            List<Thread> threads = sortByThreads(subLists);
+            mergeResult = mergeResultsFromThreads(threads, subLists);
+        } else {
+            mergeResult = null;
+        }
+        
+        if (mergeResult != null) {
+            outputSortedNumbersToFile(mergeResult);
+        } else {
+            System.out.println("Something went wrong, we don't seem to have any result...");
+        }
+
+    }
+
+    private static List<List<Integer>> createSubLists(int amountOfNumbers, int numberOfThreads) {
         List<List<Integer>> subLists = new ArrayList<>();
         try {
             FileReader fileReader = new FileReader(UNSORTED_NUMBERS_FILENAME);
@@ -43,26 +51,37 @@ public class Driver {
                 numbers.add(Integer.parseInt(line));
             }
 
-            int sizeOfSublist = AMOUNT_OF_NUMBERS/NUMBER_OF_THREADS;
-            int remainder = AMOUNT_OF_NUMBERS % NUMBER_OF_THREADS;
-            for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            int sizeOfSublist = amountOfNumbers / numberOfThreads;
+            int remainder = amountOfNumbers % numberOfThreads;
+            for (int i = 0; i < numberOfThreads; i++) {
                 int fromIndex = sizeOfSublist * i;
                 List<Integer> subList = new ArrayList<>(numbers.subList(fromIndex, fromIndex + sizeOfSublist));
 
-                if (i == NUMBER_OF_THREADS - 1 && remainder != 0) { // If AMOUNT_OF_NUMBERS/NUMBER_OF_THREADS has a remainder we have to add the extra's to the last list
-                    List<Integer> remainingNumbers = numbers.subList(AMOUNT_OF_NUMBERS - remainder, AMOUNT_OF_NUMBERS);
+                if (i == numberOfThreads - 1 && remainder != 0) { // If amountOfNumbers/numberOfThreads has a remainder we have to add the extra's to the last list
+                    List<Integer> remainingNumbers = numbers.subList(amountOfNumbers - remainder, amountOfNumbers);
                     subList.addAll(remainingNumbers);
                 }
-
-                Thread thread = new Thread(new Sorter(subList));
-                thread.start();
-                threads.add(thread);
                 subLists.add(subList);
             }
         } catch (Exception e) {
             System.out.println("Oops...exception during number reading and thread creation: " + e.getMessage());
         }
 
+        return subLists;
+    }
+
+    private static List<Thread> sortByThreads(List<List<Integer>> subLists) {
+        List<Thread> threads = new ArrayList<>();
+        for (List<Integer> subList : subLists) {
+            Thread thread = new Thread(new Sorter(subList));
+            thread.start();
+            threads.add(thread);
+        }
+
+        return threads;
+    }
+
+    private static List<Integer> mergeResultsFromThreads(List<Thread> threads, List<List<Integer>> subLists) {
         List<Integer> mergeResult = null;
         try {
             for (Thread thread : threads) {
@@ -80,23 +99,35 @@ public class Driver {
         } catch (Exception e) {
             System.out.println("Oops..exception during thread merging: " + e.getMessage());
         }
-        
-        if (mergeResult != null) {
-            try {
-                // I am not going to write random numbers into a file by hand...
-                PrintWriter printWriter = new PrintWriter(SORTED_NUMBERS_FILENAME, "UTF-8");
-                for (Integer integer : mergeResult) {
-                    printWriter.println(integer);
-                }
+        return mergeResult;
+    }
 
-                printWriter.close();
-            } catch (Exception e) {
-                System.out.println("Oops...exception during sorted number file creation: " + e.getMessage());
+    private static void outputSortedNumbersToFile(List<Integer> mergeResult) {
+        try {
+            PrintWriter printWriter = new PrintWriter(SORTED_NUMBERS_FILENAME, "UTF-8");
+            for (Integer integer : mergeResult) {
+                printWriter.println(integer);
             }
-        } else {
-            System.out.println("Something went wrong, we don't seem to have any result...");
-        }
 
+            printWriter.close();
+        } catch (Exception e) {
+            System.out.println("Oops...exception during sorted number file creation: " + e.getMessage());
+        }
+    }
+
+    private static void createRandomNumbersFile(int amountOfNumbers) {
+        try {
+            // I am not going to write random numbers into a file by hand...
+            PrintWriter printWriter = new PrintWriter(UNSORTED_NUMBERS_FILENAME, "UTF-8");
+
+            for (int i = 0; i < amountOfNumbers; i++) {
+                printWriter.println(ThreadLocalRandom.current().nextInt(0, amountOfNumbers * 10));
+            }
+
+            printWriter.close();
+        } catch (Exception e) {
+            System.out.println("Oops...exception during number generation: " + e.getMessage());
+        }
     }
 
     private static List<Integer> merge(List<Integer> left, List<Integer> right) {
