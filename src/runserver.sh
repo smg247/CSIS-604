dda511c896c6119b9616cad2786fc83c926f011b
Stephen Goeddel <sgoeddel247@gmail.com>
2016-09-20 14:33:44 -0400

    updated the script to start the socket server.

--- src/runserver.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -cp . com.stephengoeddel.distributedSorting.SortingServer 8000
---
+++ src/runsocketserver.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -cp . com.stephengoeddel.distributedSorting.sockets.SortingServer 8000
