e1327648c587d0ad0ade08dafce48317cc66ddba
Stephen Goeddel <sgoeddel247@gmail.com>
2016-09-21 00:17:48 -0400

    modified the way I get the RMI Registry.

--- src/com/stephengoeddel/distributedSorting/sorters/RMISorter.java
+++ src/com/stephengoeddel/distributedSorting/sorters/RMISorter.java
@@ -3,7 +3,8 @@ package com.stephengoeddel.distributedSorting.sorters;
 
 import com.stephengoeddel.distributedSorting.rmi.RMISortingService;
 
-import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.List;
 
 class RMISorter extends RemoteSorter {
@@ -15,7 +16,8 @@ class RMISorter extends RemoteSorter {
     @Override
     public void run() {
         try {
-            RMISortingService rmiSortingService = (RMISortingService) Naming.lookup("rmi://" + serverAddress + ":" + serverPort + "/" + RMISortingService.SERVICE_NAME);
+            Registry registry = LocateRegistry.getRegistry(serverPort);
+            RMISortingService rmiSortingService = (RMISortingService) registry.lookup(RMISortingService.SERVICE_NAME);
             numbers = rmiSortingService.sort(numbers);
         } catch (Exception e) {
             System.out.println("Encountered exception while attempting to sort with RMI: " + e.getMessage());
