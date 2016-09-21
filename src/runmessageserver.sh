c37ee619d83313ba720bfbf05d2464da3f225650
Stephen Goeddel <sgoeddel247@gmail.com>
2016-09-20 21:02:00 -0400

    updated the script to start the message server.

--- src/runmessageserver.sh
+++ src/runmessageserver.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-java -cp . com.stephengoeddel.distributedSorting.messages.MessageServer 61616
+#java -cp . com.stephengoeddel.distributedSorting.messages.MessageServer 61616
+mvn exec -Dexec.mainClass="com.stephengoeddel.distributedSorting.messages.MessageServer" -Dexec.args="60000"
