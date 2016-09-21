18dc3228124dea072fa658fa0b42e193d5f2b937
smg247 <sgoeddel247@gmail.com>
2016-09-20 22:14:21 -0400

    fixing the MessageServer.

--- runmessageserver.sh
+++ runmessageserver.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn exec:java -Dexec.mainClass="com.stephengoeddel.distributedSorting.messages.MessageServer" -Dexec.args="http://10.0.2.2 61616"
+mvn exec:java -Dexec.mainClass="com.stephengoeddel.distributedSorting.messages.MessageServer" -Dexec.args="10.0.2.2 61616"
