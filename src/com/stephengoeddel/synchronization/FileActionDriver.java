9f6f696eb5fa1b74299329f9ec6729a5fed307a5
Stephen Goeddel <sgoeddel247@gmail.com>
2016-10-04 21:12:25 -0400

    Extra safe FileActionDriver.

--- src/com/stephengoeddel/synchronization/FileActionDriver.java
+++ src/com/stephengoeddel/synchronization/FileActionDriver.java
@@ -4,6 +4,8 @@ package com.stephengoeddel.synchronization;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileActionDriver {
     private static final String FILE_NAME = "database.txt";
@@ -20,28 +22,41 @@ public class FileActionDriver {
                     if (FileAccessType.read.equals(fileAccessType)) {
                         PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
                         BufferedReader fileReader = new BufferedReader(new FileReader(FILE_NAME));
+                        List<String> linesInFile = new ArrayList<>();
                         String line;
                         while ((line = fileReader.readLine()) != null) {
-                            printWriter.println(line);
+                            linesInFile.add(line);
+                        }
+
+                        for (String lineInFile : linesInFile) {
+                            printWriter.println(lineInFile);
                         }
                         printWriter.println(".");
 
                         fileReader.close();
                         System.out.println("Transmitted the lines of " + FILE_NAME + ".");
+                        socket.close();
                     } else if (FileAccessType.write.equals(fileAccessType)) {
-                        FileWriter fileWriter = new FileWriter(FILE_NAME, true);
-                        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                        PrintWriter printWriter = new PrintWriter(bufferedWriter);
+                        FileWriter fileWriter = null;
+                        BufferedWriter bufferedWriter = null ;
+                        PrintWriter printWriter = null;
+                        try {
+                            fileWriter = new FileWriter(FILE_NAME, true);
+                            bufferedWriter = new BufferedWriter(fileWriter);
+                            printWriter = new PrintWriter(bufferedWriter);
 
-                        String line;
-                        while ((line = inputReader.readLine()) != null && !line.equals(".")) {
-                            printWriter.println(line);
+                            String line;
+                            while ((line = inputReader.readLine()) != null && !line.equals(".")) {
+                                printWriter.println(line);
+                            }
+                        } finally {
+                            printWriter.close();
+                            bufferedWriter.close();
+                            fileWriter.close();
                         }
 
-                        printWriter.close();
-                        bufferedWriter.close();
-                        fileWriter.close();
                         System.out.println("Wrote lines to " + FILE_NAME + ".");
+                        socket.close();
                     } else {
                         System.out.println("Received malformed message.");
                     }
